de4980338afe640a62886ab69c03b070697f2fa0
Andy Hine <andyhine@Andys-MacBook-Pro.local>
2017-11-12 13:34:54 -0800

    implemented the challenge

---
+++ Color Picker/Color.swift
@@ -0,0 +1,15 @@
+//
+//  Color.swift
+//  Color Picker
+//
+//  Created by Andy Hine on 11/11/17.
+//  Copyright © 2017 Andy Hine. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Color{
+    var name:String
+    var color:UIColor
+}
